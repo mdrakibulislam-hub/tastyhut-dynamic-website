a1462847e78a04ea930e1783ef6c48119ba6db2f
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-02-25 13:18:02 +0600

    api data fetched

---
+++ index.js
@@ -0,0 +1,35 @@
+const loadData = async() => {
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+    try{
+        fetch(url)
+        .then(res => res.json())
+        .then(data => showDataToCard(data.meals))
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+const showDataToCard = cardsAll =>{
+    console.log(cardsAll);
+    const cardContainer = document.getElementById('card-container')
+    
+    for (const cards of cardsAll.slice(0,11)) {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerHTML = `
+        <div class="card card-side bg-base-100 border">
+        <figure class="w-1/2"><img src="${cards.strMealThumb}" alt=""/></figure>
+                <div class="card-body w-1/2">
+                  <h2 class="card-title">${cards.strMeal}</h2>
+                  <p class="">${cards.strArea}</p>
+                  <div class="card-actions justify-end">
+                    <button class="btn btn-primary bg-[#FFC107] hover:bg-[#EEB200] border-none text-black">View Details</button>
+                  </div>
+                </div>
+                </div>
+        `
+        cardContainer.appendChild(cardDiv);
+    }
+}
+
+loadData();
